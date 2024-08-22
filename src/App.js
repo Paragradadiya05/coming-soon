21c4d2389d8df019481505f5e0149e60944a87b2
parag-radadiya <parag@ranadev.io>
2024-08-22 19:08:31 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    
+    <div className=' px-[7.5%] flex  w-[100%] h-[100vh] justify-center place-items-center'>
+            <div className=' '>
+                <h1 className='text-[14px] md:text-[17px] lg:text-[20px] xl:text-[24px] font-medium text-center'>Proxale.com is under construction. We'll be live soon!</h1>
+                <div className='grid place-items-center mt-[30px] md:mt-[40px] lg:mt-[50px]'>
+          <img width={0} height={0} className='w-[200px] md:w-[240px] lg:w-[280px] xl:w-[350px]' src='comingsoon.svg' ></img>
+          </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+
